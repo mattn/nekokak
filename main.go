58c0c3406b13ed204dcdc36a2412ed090bb0c2cd
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-09-02 16:26:50 +0900

    zoom

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"os"
 
 	"github.com/robfig/graphics-go/graphics"
+	"github.com/robfig/graphics-go/graphics/interp"
 )
 
 var (
@@ -21,6 +22,7 @@ var (
 	output  = flag.String("o", "animated.gif", "output filename")
 	reverse = flag.Bool("r", false, "inverse rotation")
 	speed   = flag.Float64("x", 1.0, "speed")
+	zoom    = flag.Bool("z", false, "zoom") // experimental
 )
 
 func main() {
@@ -54,6 +56,17 @@ func main() {
 		err = graphics.Rotate(dst, src, &graphics.RotateOptions{Angle: base * float64(i)})
 		if err != nil {
 			log.Fatal(err)
+		}
+		if *zoom {
+			w, h := float64(src.Bounds().Dx()), float64(src.Bounds().Dy())
+			tmp := image.NewPaletted(src.Bounds(), palette.WebSafe)
+			draw.Draw(tmp, src.Bounds(), &image.Uniform{color.White}, image.ZP, draw.Src)
+			z := float64(0.5 + float64(i)/30.0)
+			graphics.I.
+				Scale(z, z).
+				Translate((w-w*z)/2, (h-h*z)/2).
+				Transform(tmp, dst, interp.Bilinear)
+			dst = tmp
 		}
 		g.Image = append(g.Image, dst)
 		g.Delay = append(g.Delay, *delay)
